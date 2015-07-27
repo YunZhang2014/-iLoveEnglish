195c86ed008caeef54eaaacdb96cb0a6b02c2322
YunZhang2014 <jslgdxzy@gmail.com>
2015-07-27 23:19:15 +0930

    add entity classes

---
+++ iLoveEnglishProject/i_love_english/src/main/java/com/spark/learning/i_love_english/Entity.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package com.spark.learning.i_love_english;
+
+/**
+ * @author Yun
+ *
+ */
+public class Entity {
+   /** @pdOid ea5431f2-eb66-4134-9c10-f7b370dcfb67 */
+   private int id;
+   /** @pdOid e9635cda-11bb-49ea-9b49-2b22a6f7efe3 */
+   private java.lang.String title;
+   /** @pdOid b2fb34bd-ed12-4bad-9be4-f5bba3132aef */
+   private java.util.Date createTime;
+   
+   /** @pdOid 1ffeb0f1-b5d6-4c88-9cd2-d0e18d8b6270 */
+   public int getId() {
+      return id;
+   }
+   
+   /** @param newId
+    * @pdOid 9e43b941-01e5-4fda-b0c0-f0b67948f61a */
+   public void setId(int newId) {
+      id = newId;
+   }
+   
+   /** @pdOid c1a78b98-034e-45b5-9132-a834f32fb4d1 */
+   public java.lang.String getTitle() {
+      return title;
+   }
+   
+   /** @param newTitle
+    * @pdOid 0892365a-ec70-48bc-832c-b76c17806947 */
+   public void setTitle(java.lang.String newTitle) {
+      title = newTitle;
+   }
+   
+   /** @pdOid 681a6ede-6aae-4530-b9c7-6c7b1a329b57 */
+   public java.util.Date getCreateTime() {
+      return createTime;
+   }
+   
+   /** @param newCreateTime
+    * @pdOid 22056a6a-44e1-43ba-88b3-3f5101ba9b82 */
+   public void setCreateTime(java.util.Date newCreateTime) {
+      createTime = newCreateTime;
+   }
+
+}
---
+++ iLoveEnglishProject/i_love_english/src/main/java/com/spark/learning/i_love_english/Sentence.java
@@ -0,0 +1,19 @@
+package com.spark.learning.i_love_english;
+
+/** @pdOid 41ed7526-40a3-45bc-a991-6adeefcadd39 */
+public class Sentence extends Entity {
+   /** @pdOid fcb86c3a-4e64-4027-b2c5-39d97bfeef5e */
+   private java.lang.String content;
+   
+   /** @pdOid 3faa6184-a2e8-40ff-b9c6-9d0741137eb2 */
+   public java.lang.String getContent() {
+      return content;
+   }
+   
+   /** @param newContent
+    * @pdOid 853ee45c-bd52-42ac-b377-522184801706 */
+   public void setContent(java.lang.String newContent) {
+      content = newContent;
+   }
+
+}
